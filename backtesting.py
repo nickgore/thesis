af5043107b25ee26a84191f3acf5a66e96fe5e03
Nikanor Goreglyad <nikanor.goreglyad@creativedock.cz>
2020-07-26 23:38:34 +0200

    move plot to separate folder, fix backtest for no-exceedance cases

--- backtesting.py
+++ backtesting.py
@@ -36,41 +36,44 @@ class BackTest:
         p = (n10 + n11) / (n01 + n00 + n10 + n11)
 
         # Unconditional Coverage (Kupiec's POF test)
-        uc_h0 = (n01 + n00) * np.log(1 - self.alpha) + (n10 + n11) * np.log(self.alpha)
-        uc_h1 = (n01 + n00) * np.log(1 - p) + (n10 + n11) * np.log(p)
-        lr_pof = -2 * (uc_h0 - uc_h1)
+        if n10 + n11 > 0:
+            uc_h0 = (n01 + n00) * np.log(1 - self.alpha) + (n10 + n11) * np.log(self.alpha)
+            uc_h1 = (n01 + n00) * np.log(1 - p) + (n10 + n11) * np.log(p)
+            lr_pof = -2 * (uc_h0 - uc_h1)
 
-        # Independence (Christoffersen’s independence test)
-        ind_h0 = (n00 + n01) * np.log(1 - p) + (n01 + n11) * np.log(p)
-        ind_h1 = n00 * np.log(1 - p01) + n01 * np.log(p01) + n10 * np.log(1 - p11)
-        if p11 > 0:
-            ind_h1 += n11 * np.log(p11)
-        lr_ind = -2 * (ind_h0 - ind_h1)
+            # Independence (Christoffersen’s independence test)
+            ind_h0 = (n00 + n01) * np.log(1 - p) + (n01 + n11) * np.log(p)
+            ind_h1 = n00 * np.log(1 - p01) + n01 * np.log(p01) + n10 * np.log(1 - p11)
+            if p11 > 0:
+                ind_h1 += n11 * np.log(p11)
+            lr_ind = -2 * (ind_h0 - ind_h1)
 
-        # Conditional coverage Joint test of unconditional coverage and independence
-        # (Christoffersen’s Interval Forecast Test)
-        lr_cc = lr_pof + lr_ind
+            # Conditional coverage Joint test of unconditional coverage and independence
+            # (Christoffersen’s Interval Forecast Test)
+            lr_cc = lr_pof + lr_ind
 
-        # Stack results
-        statistics = pd.concat(
-            [
-                pd.Series([lr_pof, lr_ind, lr_cc]),
-                pd.Series(
-                    [
-                        1 - stats.chi2.cdf(lr_pof, 1),
-                        1 - stats.chi2.cdf(lr_ind, 1),
-                        1 - stats.chi2.cdf(lr_cc, 2)
-                    ]
-                ),
-                pd.Series(
-                    [
-                        test_outcome(1 - stats.chi2.cdf(lr_pof, 1), lr_pof),
-                        test_outcome(1 - stats.chi2.cdf(lr_ind, 1), lr_ind),
-                        test_outcome(1 - stats.chi2.cdf(lr_cc, 2), lr_cc)
-                    ])
-            ],
-            axis = 1
-        )
+            # Stack results
+            statistics = pd.concat(
+                [
+                    pd.Series([lr_pof, lr_ind, lr_cc]),
+                    pd.Series(
+                        [
+                            1 - stats.chi2.cdf(lr_pof, 1),
+                            1 - stats.chi2.cdf(lr_ind, 1),
+                            1 - stats.chi2.cdf(lr_cc, 2)
+                        ]
+                    ),
+                    pd.Series(
+                        [
+                            test_outcome(1 - stats.chi2.cdf(lr_pof, 1), lr_pof),
+                            test_outcome(1 - stats.chi2.cdf(lr_ind, 1), lr_ind),
+                            test_outcome(1 - stats.chi2.cdf(lr_cc, 2), lr_cc)
+                        ])
+                ],
+                axis = 1
+            )
+        else:
+            statistics = pd.DataFrame(np.zeros((3, 3))).replace(0, np.nan)
 
         # formatting
         statistics.columns = ['statistic', 'p-value', 'test outcome']
